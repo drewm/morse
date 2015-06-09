0a58b19a6f7463b3da0fa95a9297cdfab6e0b28c
drewm <drew.mclellan@gmail.com>
2015-06-09 14:11:08 +0100

    DB tests

---
+++ src/Feature/Db.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DrewM\Morse\Feature;
+
+class Db extends \DrewM\Morse\Feature
+{
+	public function testPdo()
+	{
+		return extension_loaded('pdo');
+	}
+
+	public function testPdo_Mysql()
+	{
+		// Due to historic PHP bugs, need to test PDO::MYSQL_ATTR_LOCAL_INFILE to ensure it's really available
+		return defined('PDO::MYSQL_ATTR_LOCAL_INFILE');
+	}	
+
+	public function testPdo_Sqlite()
+	{
+		return extension_loaded('pdo_sqlite');
+	}
+
+	public function testMysqli()
+	{
+		return (defined('MYSQLI_INIT_COMMAND') && class_exists('mysqli'));
+	}
+
+}
---
+++ tests/DbTest.php
@@ -0,0 +1,31 @@
+<?php
+ 
+use DrewM\Morse\Morse;
+ 
+class DbTest extends PHPUnit_Framework_TestCase {
+ 
+	public function testPdo()
+	{
+		$result = Morse::featureExists('db/pdo');
+		$this->assertTrue($result===true || $result===false);
+	}
+
+	public function testPdo_Mysql()
+	{
+		$result = Morse::featureExists('db/pdo-mysql');
+		$this->assertTrue($result===true || $result===false);
+	}
+
+	public function testPdo_Sqlite()
+	{
+		$result = Morse::featureExists('db/pdo-sqlite');
+		$this->assertTrue($result===true || $result===false);
+	}
+
+	public function testPdo_Mysqli()
+	{
+		$result = Morse::featureExists('db/mysqli');
+		$this->assertTrue($result===true || $result===false);
+	}
+
+}
